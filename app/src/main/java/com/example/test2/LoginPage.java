0aeeee647beae54d826d5d216a45a28a644cc945
이규민 <gmspell@naver.com>
2017-05-21 17:49:49 +0900

    Make a New Page(LogIn/->Server)

--- app/src/main/java/com/example/test2/LoginPage.java
+++ app/src/main/java/com/example/test2/LoginPage.java
@@ -1,10 +1,19 @@
 package com.example.test2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by 이규민 on 2017-05-18.
@@ -12,42 +21,92 @@ import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
 
-    private Button sign_in,sign_up,idFind,pwFind;
-    private EditText idField,pwField;
+    private Button sign_in,sign_up,idFind,pwFind,connect;
+    private EditText idField,pwField,server,port;
+    private LinearLayout server_page,login_page;
     String idValue,pwValue;
+
+    //-----------------------------------------------
+    private static final boolean def = true;
+    private boolean isAlived;
+    private Socket socket;
+    private ObjectOutputStream objectStream = null;
+    private Scanner scanner;
+
+//----------------------------------------------------------
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+
+        port = (EditText)findViewById(R.id.port);
+        server=(EditText)findViewById(R.id.server);
         idField = (EditText) findViewById(R.id.idField);
         pwField = (EditText) findViewById(R.id.pwField);
         sign_in = (Button) findViewById(R.id.sign_in);
         sign_up = (Button) findViewById(R.id.sign_up);
         idFind = (Button) findViewById(R.id.idFind);
         pwFind = (Button) findViewById(R.id.pwFind);
+        connect = (Button) findViewById(R.id.connect);
 
         idValue = idField.getText().toString();
         pwValue = pwField.getText().toString();
 
+        server_page = (LinearLayout) findViewById(R.id.server_page);
+        login_page = (LinearLayout) findViewById(R.id.login_page);
+
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                login_page.setVisibility(View.GONE);
+                server_page.setVisibility(View.VISIBLE);
+
+
+
+
+
+
+
 
             }
         });
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, MainActivity.class);
+                startActivity(intent);
+
+
+
+
+
+
+
+
+            }
+        });
+
+
 
         idFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                Toast.makeText(getBaseContext(), "아직 개발중입니다.", Toast.LENGTH_LONG).show();
             }
         });
-
-        pwFind.setOnClickListener();
-
-
+        pwFind.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getBaseContext(), "아직 개발중입니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+        sign_up.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getBaseContext(), "아직 개발중입니다.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
