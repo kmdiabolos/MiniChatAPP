51b6f3a6998b179fb9bc054f38410b7b950b8335
kng <kngking2@naver.com>
2017-05-23 18:54:50 +0900

    기존의 함수들을 onCreate 안에 집어넣어 오류를 해결하였음.

---
+++ MiniChatAPP-master (3)/MiniChatAPP-master/app/src/main/java/com/example/test2/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.test2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import com.example.test2.Client;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab,fab1,fab2,fab3;
+    private Animation fab_open,fab_close;
+    public boolean isFabCheck = false;
+    Client client = new Client();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                animateFAB();
+            }
+        });
+        fab1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+            //    client.start();
+                }
+            });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void animateFAB() {
+
+        if (isFabCheck) {
+
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab3.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            fab3.setClickable(false);
+            isFabCheck = false;
+            Log.d("FAB버튼", "닫힘"); // 로그를남기는 기능
+        }
+        else {
+
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab3.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            fab3.setClickable(true);
+            isFabCheck = true;
+            Log.d("FAB버튼", "열림");
+        }
+    }
+}
---
+++ MiniChatAPP-master (3)/MiniChatAPP-master/app/src/main/java/com/example/test2/Protocol.java
@@ -0,0 +1,23 @@
+package com.example.test2;
+
+import java.io.Serializable;
+
+/**
+ * Created by 이규민 on 2017-05-22.
+ */
+
+public class Protocol implements Serializable {
+    public static final long serialVersionUID  = 1;
+
+    public Protocol(String nickName){
+        this.nickName = nickName;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String nickName;
+    public String message;
+
+}
